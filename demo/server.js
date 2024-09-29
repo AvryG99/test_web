c085ebcccb5dbf54bc5454e96cdfb24a69ad70a7
AvryG99 <hagiahuy762002@gmail.com>
2024-09-30 05:57:13 +0700

    add fe fetching

--- demo/server.js
+++ demo/server.js
@@ -10,7 +10,9 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: 'https://brave-pebble-0f4a7450f.5.azurestaticapps.net' // Front-end URL
+}));
 app.use(bodyParser.json());
 
 // Function to get the database pool
